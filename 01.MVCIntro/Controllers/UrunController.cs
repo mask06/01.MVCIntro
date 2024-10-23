0616d0713f28ccf23dcc12846a69d67df5c8adfb
mask06 <Mustafaygar3903@gmail.com>
2024-10-23 16:45:15 +0300

    Add project files.

---
+++ 01.MVCIntro/Controllers/UrunController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _01.MVCIntro.Controllers
+{
+    public class UrunController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
