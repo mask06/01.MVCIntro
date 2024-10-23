166426b59032953050ba5e58433849822737ff0d
mask06 <Mustafaygar3903@gmail.com>
2024-10-23 17:21:46 +0300

    MVC Intro

--- 01.MVCIntro/Controllers/UrunController.cs
+++ 01.MVCIntro/Controllers/UrunController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _01.MVCIntro.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace _01.MVCIntro.Controllers
 {
     public class UrunController : Controller
     {
+        private static List<Urun> urunler = new List<Urun>
+        {
+            new Urun{Id=1,Ad="urun1",Fiyat = 100},
+            new Urun{Id=2,Ad="urun2",Fiyat = 200},
+            new Urun{Id=3,Ad="urun3",Fiyat = 300}
+        };
+
+
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Liste()
+        {
+            return View(urunler);
         }
     }
 }
---
+++ 01.MVCIntro/Models/Urun.cs
@@ -0,0 +1,9 @@
+﻿namespace _01.MVCIntro.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public decimal Fiyat { get; set; }
+    }
+}
